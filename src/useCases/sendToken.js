4d3ffc1feffd761c418953d0871da5ce3c673023
Kazuki Tobita <kazukiti201@gmail.com>
2023-08-25 01:04:50 +0900

    Modify answer api

--- src/useCases/sendToken.js
+++ src/useCases/sendToken.js
@@ -17,28 +17,18 @@ const ierc20Abi = JSON.parse(fs.readFileSync('./IERC20.json', 'utf-8')).abi;
 const tokenContractAddress = process.env.TOKEN_CONTRACT_ADDRESS;
 const tokenContract = new web3.eth.Contract(ierc20Abi, tokenContractAddress);
 
-// トランザクションの作成
-async function sendToken(toAddress, amount) {
+// トランザクションの作成と送金
+export default async function sendToken(toAddress, amount) {
   const gasPrice = await web3.eth.getGasPrice();
   const transferMethod = tokenContract.methods.transfer(toAddress, amount);
 
   const transaction = {
-      from: senderAccount.address,
-      to: tokenContractAddress,
-      gas: await transferMethod.estimateGas({ from: senderAccount.address }),
-      gasPrice: gasPrice,
-      data: transferMethod.encodeABI()
+    from: senderAccount.address,
+    to: tokenContractAddress,
+    gas: await transferMethod.estimateGas({ from: senderAccount.address }),
+    gasPrice: gasPrice,
+    data: transferMethod.encodeABI()
   };
 
   return web3.eth.sendTransaction(transaction);
 }
-
-const recipientAddress = '0xAd4bb1548aF5205e8B9c749296Eebe36de33c90d';
-const transferAmount = (100 * 10 ** 18).toString();
-sendToken(recipientAddress, transferAmount)
-  .then(receipt => {
-      console.log("Transaction Hash:", receipt.transactionHash);
-  })
-  .catch(error => {
-      console.error("Error sending transaction:", error);
-  });
